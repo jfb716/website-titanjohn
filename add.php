3fde53395c31241217e80ddfafd3c9e5722ade1f
John Black <john.f.black@icloud.com>
2017-02-18 12:52:47 -0500

    fixed add.php and added json.php, the form is now sending to the database and populating the table

--- add.php
+++ add.php
@@ -3,7 +3,7 @@ $conn = new mysqli("localhost","jfblack","bull1607","john_master");
 if($conn->connect_error){die("error");}
 
 $sql = $conn->prepare("INSERT INTO classes (class, site, category, complete, recommend) VALUES (?,?,?,?,?)");
-$sql->bind_param("sssds",$_POST['class'],$_POST['site'], $_POST['category'], $_POST['complete'], $_POST['recommend']);
+$sql->bind_param("sssis",$_POST['class'],$_POST['site'], $_POST['category'], $_POST['complete'], $_POST['recommend']);
 
 if($sql->execute()){
   echo 'success';
