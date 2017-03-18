cf974f9410b6728a84aa48f3ff30cbc1f299f6bd
John Black <john.f.black@icloud.com>
2017-03-18 15:55:28 -0400

    built out goals timeline with just html and bootstrap

--- siteManagement/resources/js/siteManage.js
+++ siteManagement/resources/js/siteManage.js
@@ -3,23 +3,19 @@
 
 $(document).ready(function(){
         $('.idCol').hide();
-        $('tr:odd').addClass('bg-success');
+        $('tr:odd').addClass('bg-primary');
 
         $('.sites td').blur(function(){
           var text = $(this).text();
           var id = $(this).attr('id');
           var index = $(this).index() + 1;
           var th = $('table th:nth-child(' + index + ')').attr('id');
-          console.log(text);
-          console.log(id);
-          console.log(index);
-          console.log(th);
           $.ajax({
             type: 'POST',
             url: '/siteManagement/resources/php/update.php',
             data: {text: text, id: id, th: th},
             success: function(response){
-              console.log(response);
+
             }
         });
       });
