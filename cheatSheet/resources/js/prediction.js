7983d6b9171413c5dbad62614f7c88b5c272c6fc
John Black <johnblack@fb.com>
2018-08-16 15:08:15 -0400

    adding json to CSV download tool

--- cheatSheet/resources/js/prediction.js
+++ cheatSheet/resources/js/prediction.js
@@ -1,6 +1,6 @@
 $(document).ready(function(){
 
-  $('button').click(function(){
+  $('.predictionButton').click(function(){
 
     bidReq = parseInt($('#bidRequests').val());
     matchRate = parseFloat($('#matchRate').val());
